99e70554f4c8563d46d35f544694b0c504a0fdf5
Yi Xian <exian0716@gmail.com>
2024-09-18 19:38:20 +0800

    Create main page and configurations for whole apps

---
+++ streamlit_app.py
@@ -0,0 +1,38 @@
+import streamlit as st
+from datetime import datetime
+
+
+# Setup app configurations
+st.set_page_config(
+    page_title="Visionary AI Chat",
+    page_icon="🔗",
+    layout='wide'
+)
+
+# ------ Page Setup ------
+api_usage = st.Page(
+    'pages/api_usage.py',
+    title="API Usage",
+    icon=':material/data_usage:',
+)
+
+# ------ Navigation Setup ------
+nav = st.navigation(
+    {
+        "CHATBOT": [],
+        "OTHERS": [api_usage]
+    }
+)
+
+# ------ Shared Widgets ------
+@st.cache_data
+def fetch_modified_date():
+    return datetime.now().strftime('%b %d, %Y')
+
+author = "[YX-ELITE](https://github.com/yx-elite)"
+dark_theme_logo = './static/langchain-logo-text-dark.png'
+modified_date = fetch_modified_date()
+
+st.logo(dark_theme_logo)
+st.sidebar.caption(f"Last Updated on {modified_date} by - {author} -")
+nav.run()
